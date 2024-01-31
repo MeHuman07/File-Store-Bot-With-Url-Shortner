c76a10767ef5f21a289e821ae8541ed439783455
Shubhabc123 <156100782+Shubhabc123@users.noreply.github.com>
2024-01-31 10:48:22 +0530

    Update configs.py

--- configs.py
+++ configs.py
@@ -18,12 +18,11 @@ class Config(object):
   BANNED_CHAT_IDS = list(set(int(x) for x in os.environ.get("BANNED_CHAT_IDS", "").split()))
   OTHER_USERS_CAN_SAVE_FILE = bool(os.environ.get("OTHER_USERS_CAN_SAVE_FILE", True))
   ABOUT_BOT_TEXT = f"""
-This is a Permanent FileStore Bot. 
-Send Me any Media or File. I can Work In Channel too. Add Me to Channel with Edit Permission, I will add save Uploaded File in Channel and Share a Shareable Link. 
 
-╭────[ 🔅FɪʟᴇSᴛᴏʀᴇBᴏᴛ🔅]────⍟
+
+╭────[ 🔅 STORAGE BOT 🔅]────⍟
 │
-├🔸 My Name: [FileStore Bot](https://t.me/{BOT_USERNAME})
+├🔸 My Name: [Storage Bot](https://t.me/{BOT_USERNAME})
 │        JAI SHREE RAM   
 │   BOT MADE BY TSUNAMI 
 ╰──────[ 😎 ]───────────⍟
@@ -36,11 +35,7 @@ Send Me any Media or File. I can Work In Channel too. Add Me to Channel with Edi
 [Donate Me](https://t.me/Itz_dead_soul)
 """
   HOME_TEXT = """
-Hello, [{}](tg://user?id={})\n\nThis is a Permanent **FileStore Bot**.
-
-How to Use Bot & it's Benefits??
-
-📢 Send me any File & It will be uploaded in My Database & You will Get the File Link.
-
-⚠️ Benefits: If you have a TeleGram Movie Channel or Any Copyright Channel, Then Its Useful for Daily Usage, You can Send Me Your File & I will Send Permanent Link to You & Channel will be Safe from **CopyRight Infringement** Issue. I support Channel Also You Can Check **About Bot**.
-"""
+Hello, [{}](tg://user?id={})\n\n
+We provide education video
+We share non copyrighted video's....
+  """
